d4c89137daaa5ebf348094070f6b2199bbb2ce50
Tim <timjrecker@gmail.com>
2018-01-07 15:55:17 -0800

    added i3lock

---
+++ i3/scripts/lock/lock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. /home/tim/.cache/wal/colors.sh
+convert /home/tim/.config/i3/scripts/lock/lock.png -fuzz 50% -fill "$color4" -opaque "#30AF91" /tmp/lock.png
+convert /tmp/lock.png -fuzz 10% -fill "$color0" -opaque "#FFFFFF" /tmp/lock.png
+convert /tmp/lock.png -resize 256x256 /tmp/lock.png
+
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert /tmp/screen.png /tmp/lock.png -gravity center -composite -matte /tmp/screen.png
+i3lock -u -i /tmp/screen.png
+rm /tmp/screen.png /tmp/lock.png
